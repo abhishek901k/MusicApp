6c253fe4cbed879913dd5bd24c68cb587e309608
abhishekk <abhishekk@sahaj.ai>
2025-02-02 10:35:03 +0530

    Add application-test.properties file and use it for tests

--- src/test/java/com/example/music/MusicApplicationTests.java
+++ src/test/java/com/example/music/MusicApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.music;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MusicApplicationTests {
 
 	@Test
